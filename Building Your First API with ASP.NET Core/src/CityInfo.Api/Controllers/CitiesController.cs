300bec4674e654abc885a68ae0e009f342ced12f
Marcelo Amador <marcelojaf@gmail.com>
2018-01-29 22:54:33 -0500

    Finishing module 4

---
+++ Building Your First API with ASP.NET Core/src/CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Api.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            // find city
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id);
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+        }
+    }
+}
---
+++ Building Your First API with ASP.NET Core/src/CityInfo.Api/Models/CityDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Api.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int NumberOfPointsOfInterest { get; set; }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+        = new List<PointOfInterestDto>();
+    }
+}
